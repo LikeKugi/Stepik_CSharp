fe4f557ca79e131c118e20c517f7e7fa98852a03
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-02 15:52:50 +0300

    started arithmetic

---
+++ arithmetic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace arithmetic
+{
+    public class Program{
+
+        public static void Main() {
+
+            // 1 task
+            int a = 45, b = 78;
+            int result = a * b;
+            Console.WriteLine(result);
+
+            // 2 task
+            string? inNum = Console.ReadLine();
+            double fNum = Convert.ToDouble(inNum);
+            inNum = Console.ReadLine();
+            double sNum = Convert.ToDouble(inNum);
+            Console.WriteLine("Введите числа в диапазоне от 1 до 100.");
+            Console.WriteLine("1 число: " + fNum);
+            Console.WriteLine("2 число: " + sNum);
+            Console.WriteLine(fNum / sNum);
+
+        }
+
+    }
+
+}
