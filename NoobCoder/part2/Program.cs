2be930ba5186ba04caf4a6582cdb0026d98a5b98
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-06 10:08:08 +0300

    If-Else and Switch tasks

---
+++ NoobCoder/part2/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+
+            // IF-ELSE exercises
+
+            int result = 10;
+            if(result <= 8)
+            {
+                result += 5;
+            } 
+            else
+            {
+                result -= 3;
+            }
+            Console.WriteLine(result);
+
+            // 1 task
+            string? input_num = Console.ReadLine();
+            if (input_num == null)
+            {
+                input_num = "1";
+            }
+            int num = Int32.Parse(input_num); // Запрашиваем ввод из консоли и вытягиваем из него число
+            
+            //-----------------------------------------
+            if (num % 2 == 0) // Напишите свой код вместо "..." слева
+            {
+                Console.WriteLine("EVEN");
+            }
+            else
+            {
+                Console.WriteLine("ODD");
+            }
+
+            // 2 task
+            int a1 = 0, b1 = 0;
+            string? line1 = Console.ReadLine();
+            if (line1 == null)
+            {
+                line1 = "1 1 1";
+            }
+            string[] splitString = line1.Split(' ');
+            a1 = Convert.ToInt32(splitString[0]);
+            b1 = Convert.ToInt32(splitString[1]);
+            //--------------------------------------
+            if (a1 == b1)
+            {
+                Console.WriteLine($"{a1} = {b1}");
+            } 
+            else if (a1 > b1)
+            {
+                Console.WriteLine($"{a1} > {b1}");
+            } else 
+                Console.WriteLine($"{a1} < {b1}");
+
+            // 3 task
+            int a2 = 0, b2 = 0, c2 = 0; //костыли, зато VSCode не ругается ^_^
+            string? line2 = Console.ReadLine();
+            // тоже костыли с пустой строкой
+            if (line2 == null)   
+            {
+                line2 = "1 1 1";
+            }
+            string[] splitString2 = line2.Split(' ');
+            a2 = Convert.ToInt32(splitString2[0]);
+            b2 = Convert.ToInt32(splitString2[1]);
+            c2 = Convert.ToInt32(splitString2[2]);
+            //--------------------------------------
+            int minValue2 = 0;
+            //Тут будет Ваш код
+            minValue2 = a2;
+            if (minValue2 > b2)
+            {
+                minValue2 = b2;
+            } 
+            if (minValue2 > c2)
+            {
+                minValue2 = c2;
+            }
+            Console.WriteLine(minValue2);
+            
+            // 4 task
+            string? line3 = Console.ReadLine();
+            if (line3 == null)
+                {
+                    line3 = "1 1 1 1";
+                }
+            string[] splitString3 = line3.Split(' ');
+            int a3 = Convert.ToInt32(splitString3[0]);
+            int b3 = Convert.ToInt32(splitString3[1]);
+            int c3 = Convert.ToInt32(splitString3[2]);
+            int d3 = Convert.ToInt32(splitString3[3]);
+            int minValue3 = 0;
+            int maxValue3 = 0;
+            //Тут будет Ваш код
+            minValue3 = a3;
+            maxValue3 = a3;            
+            if (minValue3 > b3)
+            {
+                minValue3 = b3;
+            } 
+            if (minValue3 > c3)
+            {
+                minValue3 = c3;
+            }
+            if (minValue3 > d3)
+            {
+                minValue3 = d3;
+            }
+            //---------------
+            if (maxValue3 < b3)
+            {
+                maxValue3 = b3;
+            } 
+            if (maxValue3 < c3)
+            {
+                maxValue3 = c3;
+            }
+            if (maxValue3 < d3)
+            {
+                maxValue3 = d3;
+            }
+            Console.WriteLine($"Наименьшее число - {minValue3}");
+            Console.WriteLine($"Наибольшее число - {maxValue3}");
+
+            // 5 task
+            double bankDeposit = Convert.ToDouble(Console.ReadLine());
+            double sum = 0.0;
+            //Тут будет Ваш код
+            if (bankDeposit > 200)
+            {
+                sum = bankDeposit * 1.1;
+            }
+            else if ((bankDeposit <= 200) && (bankDeposit >= 100))
+            {
+                sum = bankDeposit * 1.07;
+            }
+            else sum = bankDeposit * 1.05;
+
+            Console.WriteLine($"Сумма вклада после начисления процентов: {sum}");
+
+        }
+    }
+}
---
+++ NoobCoder/part2_switch/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            
+
+            // 1 task 
+            string? inpt_chr = Console.ReadLine();
+            if (inpt_chr == null)
+            {
+                inpt_chr = "а";
+            }
+            char letter = Convert.ToChar(inpt_chr);
+
+            //Тут мог быть ваш код, но его пока нет.
+
+            switch (letter)
+            {
+                case 'а':
+                    Console.WriteLine("гласная");
+                break;
+                case 'у':
+                    Console.WriteLine("гласная");
+                break;
+                case 'е':
+                    Console.WriteLine("гласная");
+                break;
+                case 'ё':
+                    Console.WriteLine("гласная");
+                break;
+                case 'ы':
+                    Console.WriteLine("гласная");
+                break;
+                case 'о':
+                    Console.WriteLine("гласная");
+                break;
+                case 'э':
+                    Console.WriteLine("гласная");
+                break;
+                case 'я':
+                    Console.WriteLine("гласная");
+                break;
+                case 'и':
+                    Console.WriteLine("гласная");
+                break;
+                case 'ю':
+                    Console.WriteLine("гласная");
+                break;
+            }
+
+            // 2 task
+            string? inpt_day = Console.ReadLine();
+            if (inpt_day == null)
+            {
+                inpt_day = "5";
+            }
+            int dayNumber = Convert.ToInt32(inpt_day);
+            //----------------------------------------
+            dayNumber = dayNumber % 7;
+
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Понедельник");
+                break;
+                case 2:
+                    Console.WriteLine("Вторник");
+                break;
+                case 3:
+                    Console.WriteLine("Среда");
+                break;
+                case 4:
+                    Console.WriteLine("Четверг");
+                break;
+                case 5:
+                    Console.WriteLine("Пятница");
+                break;
+                case 6:
+                    Console.WriteLine("Суббота");
+                break;
+                case 0:
+                    Console.WriteLine("Воскресенье");
+                break;
+            }
+
+            // 3 task
+            string? inpt_opr = Console.ReadLine();
+            if (inpt_opr == null)
+            {
+                inpt_opr = "5";
+            }
+            int operation3 = Convert.ToInt32(inpt_opr);
+            //----------------------------------------
+            Console.WriteLine("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение");
+            switch (operation3)
+            {
+                case 1:
+                    Console.WriteLine("Сложение");
+                break;
+                case 2:
+                    Console.WriteLine("Вычитание");
+                break;
+                case 3:
+                    Console.WriteLine("Умножение");
+                break;
+                default:
+                    Console.WriteLine("Неизвестная операция!");
+                break;
+            }
+
+            // 4 task
+            string? line = Console.ReadLine();
+            if (line == null)
+            {
+                line = "1 2 3";
+            }
+            string[] splitString = line.Split(' ');
+
+            int operation = Convert.ToInt32(splitString[0]);
+            double a = Convert.ToDouble(splitString[1]);
+            double b = Convert.ToDouble(splitString[2]);
+            double result = 0;
+            //-----------------------------------------------
+            Console.WriteLine("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение");
+            switch (operation)
+            {
+                case 1:
+                    {
+                    result = a + b;
+                    Console.WriteLine($"Результат операции {result}");
+                    }
+                break;
+                case 2:
+                    {
+                    result = a - b;
+                    Console.WriteLine($"Результат операции {result}");
+                    }
+                break;
+                case 3:
+                    {
+                    result = a * b;
+                    Console.WriteLine($"Результат операции {result}");
+                    }                
+                break;
+                default:
+                    Console.WriteLine("Неизвестная операция!");
+                break;
+            }
+
+            /* OPERATOR '?' EXAMPLE */
+            int x = 5;
+            int y = 3;
+            int z = x < y ? (x + y) : (x - y);
+            /*
+            *   if (x < y)
+            *   {
+            *    z = x + y
+            *   }
+            *   else z = x - y
+            *
+            *   IF (term) --> (smth);
+            *   ELSE --> (smth)
+            */
+
+            // 5 task
+            string? num_odd_even = Console.ReadLine();
+            if (num_odd_even == null)
+            {
+                num_odd_even = "2";
+            }
+            int num = Int32.Parse(num_odd_even); // Запрашиваем ввод из консоли и вытягиваем из него число
+            //Пишите тут.                
+            string answer = (num % 2 != 0) ? "ODD" : "EVEN"; 
+            Console.WriteLine(answer);
+
+            // 6 task
+            Console.WriteLine("Введите два числа через пробел: длина и ширина участка");
+            string? line6 = Console.ReadLine();
+            if (line6 == null)
+            {
+                line6 = "12 13";
+            }
+            string[] splitString6 = line6.Split(' ');
+            double length = Convert.ToDouble(splitString6[0]); // длина
+            double width = Convert.ToDouble(splitString6[1]); // ширина
+            double landArea = 0;                             // переменная для площади
+            //Тут будет Ваш код
+
+            int times_length = (int)Math.Ceiling(length / 5);
+            int times_width = (int)Math.Ceiling(width / 5);
+            //Console.WriteLine($"length = {length} times = {times_length}");
+            //Console.WriteLine($"width = {width} times = {times_width}");
+            length -= times_length * 0.2;
+            width -= times_width * 0.2;
+
+            landArea = Math.Round((length * width), 2);
+
+
+            Console.WriteLine($"Площадь участка: {landArea}");
+        }
+    }
+}
