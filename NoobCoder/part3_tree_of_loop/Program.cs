a0f7132a98bd493ef14398f0662b2b556a8935ba
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-07 09:33:10 +0300

    painted a tree in console using *

---
+++ NoobCoder/part3_tree_of_loop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+
+            string? inpt_height = Console.ReadLine();
+            if (inpt_height == null) {
+                inpt_height = "10";
+            }
+            int height = int.Parse(inpt_height);
+
+            //Пространство для вашего творчества
+            int middle = height;
+            int counter_height = 0; // current height
+            int counter_width; // current pos in line
+            
+            //loop for height, where i == counter_height?
+            for (int i = 0; i < height; i++) {
+                counter_height = i;
+                counter_width = 0;
+                while (counter_width <= (middle + counter_height - 1)) {
+                    if (counter_width < (middle - counter_height - 1)) {
+                        Console.Write(" ");
+                    }
+                    else if ((counter_width >= (middle - counter_height - 1)) && (counter_width <= (middle + counter_height -1))) {
+                        Console.Write("*");
+                    }
+                    else {
+                        break;
+                    }
+                    counter_width++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
