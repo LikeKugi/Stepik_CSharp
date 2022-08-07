310fe626a00df1c5a0c5d72d1e4d617464fb5aec
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-07 13:19:30 +0300

    task with minutes. didn't understand but was very interesting

---
+++ NoobCoder/part3_minutes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+
+            // 60*(n+1)/11
+            string? inpt_tms = Console.ReadLine();
+            if (inpt_tms == null) {
+                inpt_tms = "5";
+            }
+            int n = int.Parse(inpt_tms);
+
+            //Ваш код
+
+            int minutes_left;
+            minutes_left = 12 * 60 + 30;
+            int minutes_past = 60 * (n + 1) / 11;
+            int current_time = minutes_left + minutes_past;
+            int current_hours = current_time / 60;
+            int current_minutes = current_time % 60;
+
+            Console.WriteLine($"{current_hours}:{current_minutes}");
+        }
+    }
+}
