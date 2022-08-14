f2096e9738fb23a509d2963e1c0b84748893cdfa
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-14 09:39:30 +0300

    lol kek chebureck. я не знаю, как описать эту задачу. декодинг какой-то непонятный

---
+++ NoobCoder/part6_methods/ex4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void AhShitHereWeGoAgain(int[] arr, int minValue)
+        {
+            int[] sortArr = new int[arr.Length + 1];
+            Array.Copy(arr, 0, sortArr, 1, sortArr.Length-1);
+            foreach (int el in sortArr){
+                Console.Write(el + " ");
+            }
+            //Console.WriteLine();
+            for (int i = 1; i < sortArr.Length; i++) {
+                sortArr[i] += sortArr[i-1];
+            }
+            foreach (int el in sortArr){
+                Console.Write(el + " ");
+            }
+            //Console.WriteLine();
+            int min = System.Linq.Enumerable.Min(sortArr);
+            //Console.WriteLine(min);
+            
+            for (int i = 0; i < sortArr.Length; i++) {
+                sortArr[i] += minValue - min;
+            }
+            foreach (int el in sortArr){
+                Console.Write(el + " ");
+            }
+        }
+        static int GetDecrypt(int val, int min, int minVal)
+        {
+            return val += minVal - min;
+        }
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            int hourHandAmount = 1468;        
+            int[] vasyaArray = {27,-15,2,22,12,-55,11};        
+            AhShitHereWeGoAgain (vasyaArray, hourHandAmount);
+        }
+    }
+}
