0da23444515897f7d0e103626b944b8396968c90
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-18 12:37:18 +0300

    class revers and to upper string

--- NoobCoder/part9_classes/ex2/Program.cs
+++ NoobCoder/part9_classes/ex2/Program.cs
@@ -1,14 +1,58 @@
 ﻿using System;
+using System.Linq;
 
 namespace MyProgram
 {
     class Program
     {
+        static string GetLine()
+        {
+            string? line = Console.ReadLine();
+            if (string.IsNullOrEmpty(line))
+                line = "hello world!";
+            return line;
+        }
         static void Main(string[] args)
         {
-        // Main starts here
+            
             Console.Clear();
+
+            string line = GetLine();
+            var myS = new MyString();
+            myS.line = line;
+
+            Console.WriteLine(myS.Reverse());
+            Console.WriteLine(myS.UcFirst());
+            Console.WriteLine(myS.UcWords());
+
 
         }
     }
+
+    class MyString
+    {
+        public string? line;
+
+        public string Reverse() => new string(line.Reverse().ToArray());
+        public string UcFirst() => (string.IsNullOrEmpty(line)) ? line : (line[0].ToString().ToUpper() + line.Substring(1));
+        
+
+        public string UcWords() 
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {   
+                //Console.WriteLine("empty");
+                return line;
+            }
+            string returned = "";
+            var words = line.Trim().Split(' ');
+            foreach (string word in words)
+            {
+                returned += (word[0].ToString().ToUpper() + word.Substring(1)) + " ";
+            }
+            return returned;
+        }
+
+    }
+
 }
