90b4d3ecc9492902bc5f19450c286946c29e25cb
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-21 15:44:52 +0300

    check for triangle

---
+++ Essentials/Part_2/ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static int GetNumber() => Convert.ToInt32(Console.ReadLine());
+        static int[] GetValues()
+        {
+            var values = new int[3];
+            for (int i = 0; i< values.Length;i++)
+            {
+                values[i] = GetNumber();
+            }
+            return values;
+        }
+        static string CheckTriangle(int[] values)
+        {
+            return (values.Max() >= (Enumerable.Aggregate(values, (a,x)=> a+=x))-values.Max()) ? "no" : "yes";
+        }
+
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            
+            var length = GetValues();
+            Console.WriteLine(CheckTriangle(length));
+
+        }
+    }
+}
