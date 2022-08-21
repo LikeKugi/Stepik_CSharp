54b4a928a1b7547ac4bb6dd48046104a2f1cf821
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-21 15:28:11 +0300

    if-else check age

---
+++ Essentials/Part_2/ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static int GetNumber() => Convert.ToInt32(Console.ReadLine());
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            var age = GetNumber();
+
+            string output = (age < 18) ? "ребёнок" : (age >= 70) ? "старик" : "взрослый";
+            Console.WriteLine(output);
+
+        }
+    }
+}
