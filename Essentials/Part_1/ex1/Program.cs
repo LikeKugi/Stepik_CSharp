36d37bbf51f85fafbb3dcfa2f87469b639161561
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-21 11:05:03 +0300

    Multiply 2 numbers

---
+++ Essentials/Part_1/ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {   
+        static int GetInt() => Convert.ToInt32(Console.ReadLine());
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            int a = GetInt();
+            int b = GetInt();
+
+            Console.WriteLine(a*b);
+
+        }
+    }
+}
