0ce2def7da981d9cb3ad55424a9fcf765bee2c48
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-22 09:41:18 +0300

    check for avaiable cut off some squares of list

---
+++ Essentials/Part_2/ex9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyProgram
+{
+    
+    class Program
+    {
+        static int GetNumber() => Convert.ToInt32(Console.ReadLine());
+        static int[] GetValues()
+        {
+            var values = new int[3];
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = GetNumber();
+            }
+            return values;
+        }
+        static string CheckAvaiable(int[] coords)
+        {
+            bool dimensions = (coords[0] * coords[1]) >= coords[2];
+            bool numbers = (coords[2] % coords[0]) == 0 || (coords[2] % coords[1]) == 0;
+            return (numbers && dimensions)  ? "yes" : "no";
+        }
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            Console.WriteLine(CheckAvaiable(GetValues()));
+
+        }
+    }
+}
