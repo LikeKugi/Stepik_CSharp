b601ad161fe5eda8ab975a5ab3cfa7dcee2cb77e
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-22 08:50:15 +0300

    chessmate cheks for avaiable move

---
+++ Essentials/Part_2/ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static int GetNumber() => Convert.ToInt32(Console.ReadLine());
+        static int[] GetValues()
+        {
+            var values = new int[4];
+            for (int i = 0; i< values.Length;i++)
+            {
+                values[i] = GetNumber();
+            }
+            return values;
+        }
+        static string CheckAvaiable(int[] coords)
+        {
+            return (coords[0]==coords[2]||coords[1]==coords[3])? "yes":"no";
+        }
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            Console.WriteLine(CheckAvaiable(GetValues()));
+
+        }
+    }
+}
---
+++ Essentials/Part_2/ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static int GetNumber() => Convert.ToInt32(Console.ReadLine());
+        static int[] GetValues()
+        {
+            var values = new int[4];
+            for (int i = 0; i< values.Length;i++)
+            {
+                values[i] = GetNumber();
+            }
+            return values;
+        }
+        static string CheckAvaiable(int[] coords)
+        {
+            return (Math.Abs(coords[0]-coords[2])<2||Math.Abs(coords[1]-coords[3])<2)? "yes":"no";
+        }
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            Console.WriteLine(CheckAvaiable(GetValues()));
+
+        }
+    }
+}
