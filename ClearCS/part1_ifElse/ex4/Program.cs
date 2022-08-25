cc83dc9ae6c9b31194a25511e576a44ac15e73f6
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-25 09:01:31 +0300

    if-else-switch states added

---
+++ ClearCS/part1_ifElse/ex4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            var month = GetMonth();
+            //month = int.MinValue;
+            PrintMonth(month);
+
+        }  
+        static int GetMonth() => Convert.ToInt32(Console.ReadLine());
+        static void PrintMonth(int month)
+        {
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Январь");
+                    break;
+                case 2:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 3:
+                    Console.WriteLine("Март");
+                    break;
+                case 4:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 5:
+                    Console.WriteLine("Май");
+                    break;
+                case 6:
+                    Console.WriteLine("Июнь");
+                    break;
+                case 7:
+                    Console.WriteLine("Июль");
+                    break;
+                case 8:
+                    Console.WriteLine("Август");
+                    break;
+                case 9:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 12:
+                    Console.WriteLine("Декабрь");
+                    break;
+            }
+        }
+    }
+}
---
+++ ClearCS/part1_ifElse/ex5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main starts here
+            Console.Clear();
+            var x = GetNumber();
+            var y = GetNumber();
+
+            switch (FindAnswer(x * y))
+            {
+                case 1:
+                    Console.WriteLine(FindPLusS(x, y)); 
+                    break;
+                case 0:
+                    Console.WriteLine(FindZeroS(x, y)); 
+                    break;
+                case -1:
+                    Console.WriteLine(FindMinusS(x, y)); 
+                    break;
+            }
+
+        }
+        static int GetNumber() => Convert.ToInt32(Console.ReadLine());
+        static int FindAnswer(int a) => (a > 0) ? 1 : (a < 0) ? -1 : 0;
+        static int FindPLusS(int x, int y) => 2 * x + y;
+        static int FindZeroS(int x, int y) => 0;
+        static int FindMinusS(int x, int y) => 2 * Math.Abs(x) - y;
+    }
+}
