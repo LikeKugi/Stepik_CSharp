0e453a4a51d6f2a3bd399aa976b8205d7146754a
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-08-25 11:31:33 +0300

    loops wtf tasks

---
+++ ClearCS/part2_loops/ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Main starts here
+            Console.Clear();
+            var countEnemies = GetIntNumber();
+            var countHeads = GetIntNumber();
+            var sum = countEnemies*countHeads;
+            var days = 0;
+            for (int i = 0; i < sum; i+=3)
+            {
+                
+                days++;
+            }
+            Console.WriteLine(days + " дня");
+
+        }
+        static int GetIntNumber() => Convert.ToInt32(Console.ReadLine());
+    }
+}
